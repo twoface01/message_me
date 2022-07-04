4eb52c9000692cf96b95c690908dfb6e8b27d6d4
Chris Jones <twoface01@gmail.com>
2022-07-04 21:02:08 +0000

    Chatroom messaging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,26 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function () {
+  if ($('#chatroom').length > 0) {
+    $('#chatroom').scrollTop($('#chatroom')[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    }
+  });
+}
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
-  
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
+  scroll_bottom();
 });
