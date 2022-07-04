8fe3e6fa7f974a065f70727287de5a3d6c059784
Chris Jones <twoface01@gmail.com>
2022-07-04 17:26:01 +0000

    Basic User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    # return current user if it exists, otherwise query the database
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    # bang bang turns the value to a boolean
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must be logged in to perform this action."
+      redirect_to login_path
+    end
+  end
+    
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,20 @@ class SessionsController < ApplicationController
   end
   
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have logged in successfully!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Your login credentials were not accepted. Please try again."
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out successfully."
+    redirect_to login_path
   end
 end
