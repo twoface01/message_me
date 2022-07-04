ff7d1205112a51a4e33cc9b5bf0aa8e2532505b3
Chris Jones <twoface01@gmail.com>
2022-07-04 17:36:18 +0000

    Flash messaging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,9 @@
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
-  $('.ui.dropdown')
-    .dropdown()
-  ;    
+  $('.ui.dropdown').dropdown();
+  
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 });
